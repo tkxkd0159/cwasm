1e43d96a73eb0b449eda6bae6e2e9c5ab9f78d6f
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2023-06-23 02:11:42 +0900

    test: more settings with `App::new`

--- contracts/cw-start/src/lib.rs
+++ contracts/cw-start/src/lib.rs
@@ -8,8 +8,8 @@ pub mod state;
 
 #[cfg(test)]
 mod tests {
-    use cosmwasm_std::{Addr, Attribute, Empty};
-    use cw_multi_test::{Contract, ContractWrapper, App, Executor};
+    use cosmwasm_std::{Addr, Attribute, Empty, coins};
+    use cw_multi_test::{Contract, ContractWrapper, App, Executor, Module};
     use crate::contract::{instantiate, execute, query};
     use crate::query::{EnvResponse};
     use crate::msg::{QueryMsg, InstantiateMsg, ExecuteMsg};
@@ -36,16 +36,19 @@ mod tests {
 
     #[test]
     fn exec_increment() {
-        let mut app = App::default();
+        let sender = Addr::unchecked("sender");
+        let mut app = App::new(|router, api, storage| {
+            router.bank.init_balance(storage, &sender, coins(10, "stake")).unwrap();
+        });
         let contract_id = app.store_code(mock_contract());
-        let contract_addr = app.instantiate_contract(contract_id, Addr::unchecked("sender"), &InstantiateMsg{desc: "exec_increment".to_string()}, &[], "Starting contract", None).unwrap();
+        let contract_addr = app.instantiate_contract(contract_id, sender.clone(), &InstantiateMsg{desc: "exec_increment".to_string()}, &[], "Starting contract", None).unwrap();
 
-        let resp = app.execute_contract(Addr::unchecked("sender"), contract_addr.clone(), &ExecuteMsg::Increment {}, &[]).unwrap();
+        let resp = app.execute_contract(sender.clone(), contract_addr.clone(), &ExecuteMsg::Increment {}, &[]).unwrap();
         assert_eq!(resp.events[0].ty, "execute");
         assert_eq!(resp.events[1].ty, "wasm");
         assert_eq!(resp.events[1].attributes, vec![
             Attribute{key: "_contract_addr".to_string(), value: "contract0".to_string()},
-            Attribute::new("owner", "sender"),
+            Attribute::new("owner", sender),
             Attribute::new("count", "1"),
         ]);
     }
